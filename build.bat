71b60186031f4dfbb2ea35cd725c51c60baa91ee
J0n-b0 <119065467+J0n-b0@users.noreply.github.com>
2024-11-12 19:32:57 -0500

    usr2arm9ldr -> just2arm9ldr

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 cd ID1gen && make clean && make && cd..
 python id1gen.py
-cd usr2arm9ldr && make clean && make && cd ..
-cp usr2arm9ldr/build/arm9.bin out/b9
+cd just2arm9ldr && make clean && make && cd ..
+cp just2arm9ldr/build/arm9.bin out/b9
 pause
